fd6235f7ff07bb2d280f593cbef4412d72edd42a
Nguyen Hoa Khang <111685264+P1emon@users.noreply.github.com>
2024-11-21 03:36:14 +0700

    Second commit

---
+++ MyEStore/MyEStore/Program.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.EntityFrameworkCore;
+using MyEStore.Entities;
+using MyEStore.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddDbContext<MyeStoreContext>(options => {
+	options.UseSqlServer(builder.Configuration.GetConnectionString("MyDb"));
+});
+
+builder.Services.AddDistributedMemoryCache();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(20);
+});
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.ExpireTimeSpan = TimeSpan.FromMinutes(20);
+        options.SlidingExpiration = true;
+        options.LoginPath = "/Customer/Login";
+        options.AccessDeniedPath = "/Forbidden/";
+    });
+
+//đăng ký PaymentClient dạng Singleton
+builder.Services.AddSingleton(x =>
+	new PaypalClient(
+		builder.Configuration["PayPalOptions:ClientId"],
+		builder.Configuration["PayPalOptions:ClientSecret"],
+		builder.Configuration["PayPalOptions:Mode"]
+	)
+);
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+	app.UseExceptionHandler("/Home/Error");
+	// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+	app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseSession();
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllerRoute(
+	name: "default",
+	pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();

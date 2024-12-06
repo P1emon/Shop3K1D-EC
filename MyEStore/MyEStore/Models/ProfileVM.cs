54d2a3a67136dadc08d747872009001fee25802a
Duc Truong <truongduc500@gmail.com>
2024-12-06 10:10:14 +0700

    Profile, Menu

--- MyEStore/MyEStore/Models/ProfileVM.cs
+++ MyEStore/MyEStore/Models/ProfileVM.cs
@@ -6,6 +6,8 @@
         public string FullName { get; set; } = null!;
         public string Email { get; set; } = null!;
         public string Password { get; set; } = null!;
+        public string NewPassword { get; set; }
+        public string ConfirmPassword { get; set; }
         public string? Phone { get; set; }
     }
 }

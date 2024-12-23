d0732eda21a3c70a3363e9cd3f8f5f45d7084b90
P1emon <khang113324@gmail.com>
2024-12-24 05:50:20 +0700

    Cập nhật thanh toán momo 85%
    Do Momo UAT của tui bị lỗi nên chưa check kỹ được nhưng chung quy là có thể tiến hành thanh toán được rồi

--- MyEStore/MyEStore/Controllers/PaymentController.cs
+++ MyEStore/MyEStore/Controllers/PaymentController.cs
@@ -3,7 +3,10 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using MyEStore.Entities;
 using MyEStore.Models;
+using Newtonsoft.Json;
 using System.Security.Claims;
+using System.Security.Cryptography;
+using System.Text;
 using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace MyEStore.Controllers
@@ -13,15 +16,128 @@ namespace MyEStore.Controllers
 	{
 		private readonly PaypalClient _paypalClient;
 		private readonly MyeStoreContext _ctx;
+        private readonly IConfiguration _configuration;
 
-		public PaymentController(PaypalClient paypalClient, MyeStoreContext ctx)
-		{
-			_paypalClient = paypalClient;
-			_ctx = ctx;
+        public PaymentController(PaypalClient paypalClient, MyeStoreContext ctx, IConfiguration configuration)
+        {
+            _paypalClient = paypalClient;
+            _ctx = ctx;
+            _configuration = configuration;
+        }
 
-		}
+        [HttpPost]
+        public IActionResult MomoPayment()
+        {
+            // Lấy thông tin từ cấu hình
+            var endpoint = _configuration["MoMo:Endpoint"];
+            var partnerCode = _configuration["MoMo:PartnerCode"];
+            var accessKey = _configuration["MoMo:AccessKey"];
+            var secretKey = _configuration["MoMo:SecretKey"];
+            var returnUrl = _configuration["MoMo:ReturnUrl"];
+            var notifyUrl = _configuration["MoMo:NotifyUrl"];
 
-		public IActionResult Index()
+            // Tính tổng tiền (VND)
+            var tongTien = CartItems.Sum(p => p.ThanhTien);
+
+            // Tạo các tham số thanh toán
+            var orderInfo = "Thanh toán đơn hàng tại MyEStore";
+            var requestId = Guid.NewGuid().ToString();
+            var orderId = "DH" + DateTime.Now.Ticks.ToString();
+            var extraData = "";
+
+            // Tạo chữ ký (signature)
+            string rawHash = $"accessKey={accessKey}&amount={tongTien}&extraData={extraData}&ipnUrl={notifyUrl}&orderId={orderId}&orderInfo={orderInfo}&partnerCode={partnerCode}&redirectUrl={returnUrl}&requestId={requestId}&requestType=captureWallet";
+            string signature = GenerateSignature(rawHash, secretKey);
+
+            // Tạo body yêu cầu
+            var body = new
+            {
+                partnerCode = partnerCode,
+                accessKey = accessKey,
+                requestId = requestId,
+                amount = tongTien.ToString(),
+                orderId = orderId,
+                orderInfo = orderInfo,
+                redirectUrl = returnUrl,
+                ipnUrl = notifyUrl,
+                extraData = extraData,
+                requestType = "captureWallet",
+                signature = signature
+            };
+
+            using (var client = new HttpClient())
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(body), Encoding.UTF8, "application/json");
+                var response = client.PostAsync(endpoint, content).Result;
+
+                if (response.IsSuccessStatusCode)
+                {
+                    var responseContent = response.Content.ReadAsStringAsync().Result;
+                    dynamic jsonResponse = JsonConvert.DeserializeObject(responseContent);
+                    string payUrl = jsonResponse.payUrl;
+
+                    return Redirect(payUrl); // Chuyển hướng đến MoMo để thanh toán
+                }
+                else
+                {
+                    return BadRequest("Không thể tạo yêu cầu thanh toán MoMo.");
+                }
+            }
+        }
+        public IActionResult MomoReturn()
+        {
+            var queryString = Request.Query;
+            string resultCode = queryString["resultCode"];
+            string orderId = queryString["orderId"];
+            string message = queryString["message"];
+
+            if (resultCode == "0")
+            {
+                ViewBag.Message = "Thanh toán thành công!";
+                ViewBag.OrderId = orderId;
+                return View("Success");
+            }
+            else
+            {
+                ViewBag.Message = "Thanh toán thất bại: " + message;
+                ViewBag.OrderId = orderId;
+                return View("MomoFail");
+            }
+        }
+
+        private static string GenerateSignature(string data, string secretKey)
+        {
+            var encoding = Encoding.UTF8;
+            using (var hmacsha256 = new HMACSHA256(encoding.GetBytes(secretKey)))
+            {
+                byte[] hashmessage = hmacsha256.ComputeHash(encoding.GetBytes(data));
+                return BitConverter.ToString(hashmessage).Replace("-", "").ToLower();
+            }
+        }
+
+
+        [HttpPost]
+        public IActionResult MomoNotify()
+        {
+            using (var reader = new StreamReader(Request.Body))
+            {
+                var body = reader.ReadToEnd();
+                dynamic jsonBody = JsonConvert.DeserializeObject(body);
+
+                if (jsonBody.resultCode == "0")
+                {
+                    // Xử lý đơn hàng thành công
+                    return Ok();
+                }
+                else
+                {
+                    // Xử lý thất bại
+                    return BadRequest();
+                }
+            }
+        }
+
+        public IActionResult Index()
 		{
 			ViewBag.PaypalClientId = _paypalClient.ClientId;
 			return View(CartItems);
